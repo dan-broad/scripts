641b22f8acbc1250930239addcebfbd5567857e6
dan-broad <94935380+dan-broad@users.noreply.github.com>
2022-01-20 19:36:16 -0500

    Update untargz.sh

--- untargz.sh
+++ untargz.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir /home/jupyter/notebooks/tmp
-gsutil cp $1 /home/jupyter/notebooks/tmp/compressed
+gsutil cp $1 /home/jupyter/notebooks/tmp/compressed/
 for filename in /home/jupyter/notebooks/tmp/compressed/*.tar.gz; do
-  tar -zxf $filename -C home/jupyter/notebooks/tmp/decompressed     
+  tar -zxf $filename -C home/jupyter/notebooks/tmp/decompressed/     
 done
-gsutil cp -rm /home/jupyter/notebooks/tmp/decompressed $2
-rm -rf /home/jupyter/notebooks/tmp
+gsutil cp -rm /home/jupyter/notebooks/tmp/decompressed/ $2
+rm -rf /home/jupyter/notebooks/tmp/
